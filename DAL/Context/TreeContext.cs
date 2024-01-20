63b37fc4730c73f5a878ab84c0a463d7711c577f
AMSaiian <artursaiyan56@gmail.com>
2024-01-20 14:58:57 +0200

    Implemented db context and create init migration

---
+++ DAL/Context/TreeContext.cs
@@ -0,0 +1,9 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context;
+
+public class TreeContext(DbContextOptions<TreeContext> options) : DbContext(options)
+{
+    public required DbSet<Node> Nodes { get; set; }
+}
---
+++ DAL/Entities/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities;
+
+[Table(nameof(Node))]
+public class Node
+{
+    [Key]
+    [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    [Required(AllowEmptyStrings = false)]
+    public required string ConfigName { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    public required string Name { get; set; }
+    
+    public string? Value { get; set; }
+    
+    [ForeignKey("Parent")]
+    public Guid? ParentId { get; set; }
+    
+    public Node? Parent { get; set; }
+    
+    public IList<Node> Children { get; set; } = new List<Node>();
+}
